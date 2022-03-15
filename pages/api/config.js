db6fc68f0f2b218ecc17b22bf9bcd2caa101e0b9
hemcharan <77770865+hemcharan22@users.noreply.github.com>
2022-03-15 19:48:31 +0530

    Update config.js

--- pages/api/config.js
+++ pages/api/config.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 
 export const config = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  apiKey: AIzaSyBawGg0SI3tQfzDDn9qJS_vc6eCLrOkHys,
+  authDomain: adept-amp-324009.firebaseapp.com,
+  projectId: adept-amp-324009,
+  storageBucket: adept-amp-324009.appspot.com,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  appId: 1:594218645311:web:9f8eb6494a1a5431bb858a,
+  measurementId: G-6Q8L17CTNX,
 };
 
 
