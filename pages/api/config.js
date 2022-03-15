7ebe9c702e69ed030f2622d72ee59c3b2c6d0384
hemcharan <77770865+hemcharan22@users.noreply.github.com>
2022-03-15 19:51:46 +0530

    Update config.js

--- pages/api/config.js
+++ pages/api/config.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 
 export const config = {
-  apiKey: AIzaSyBawGg0SI3tQfzDDn9qJS_vc6eCLrOkHys,
-  authDomain: adept-amp-324009.firebaseapp.com,
-  projectId: adept-amp-324009,
-  storageBucket: adept-amp-324009.appspot.com,
-  messagingSenderId: 594218645311,
-  appId: 1:594218645311:web:9f8eb6494a1a5431bb858a,
-  measurementId: G-6Q8L17CTNX,
+  apiKey: 'AIzaSyBawGg0SI3tQfzDDn9qJS_vc6eCLrOkHys',
+  authDomain: 'adept-amp-324009.firebaseapp.com',
+  projectId: 'adept-amp-324009',
+  storageBucket: 'adept-amp-324009.appspot.com',
+  messagingSenderId: '594218645311',
+  appId: '1:594218645311:web:9f8eb6494a1a5431bb858a',
+  measurementId: 'G-6Q8L17CTNX',
 };
 
 
