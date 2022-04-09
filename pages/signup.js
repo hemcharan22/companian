885cb5f17ad0332c293e3fedb66caf7575d259d3
hemcharan <hemcharan54@gmail.com>
2022-04-09 20:46:39 +0530

    changes

--- pages/signup.js
+++ pages/signup.js
@@ -2,7 +2,6 @@ import Link from "next/link";
 import { useState } from "react";
 import { useRouter } from "next/router";
 
-
 import { useAuth } from "../pages/api/auth";
 import { setErrorMessage } from "../pages/api/setErrorMessage";
 
